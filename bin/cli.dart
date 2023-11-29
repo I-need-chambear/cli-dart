d2e3a0a030b868f9c75a1f1eb23369ff9c15eb06
JohnGolgota <js684new@gmail.com>
2023-11-28 20:35:17 -0500

    pruebas

--- bin/cli.dart
+++ bin/cli.dart
@@ -3,4 +3,5 @@ import 'package:cli/cli.dart' as cli;
 void main(List<String> arguments) {
   print('Hello world: $arguments!');
   print('Hello world: ${cli.calculate()}!');
+  print('Hello world: ${cli.calculate()}!');
 }
